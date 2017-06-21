e29d0de078f8c010c513a06a083608157c49fc2b
CarlHeinst <cheinst@gmail.com>
2017-06-21 12:47:14 +0100

    Update paraping.sh

--- paraping.sh
+++ paraping.sh
@@ -9,3 +9,13 @@ AS MANY OTHER IPS as you like
 ' | sed 's/0.0.0.0\n\|0.0.0.0//g' \
 | xargs -n 1 -P $para_count -I{} bash -c "ping {} -c $ping_count \
 | sed -e '1i\START={}' -e '$ a\FINISH'" | tee /tmp/ping_res.txt
+
+### Pinging a list of IPs in a file:
+
+para_count=10
+## This is the number of paralell processes to instruct Xargs to make use of
+ping_count=4 
+## This is the number of pings to carry out
+cat /tmp/file_of_ips.txt | sed 's/0.0.0.0\n\|0.0.0.0//g' \
+| xargs -n 1 -P $para_count -I{} bash -c "ping {} -c $ping_count \
+| sed -e '1i\START={}' -e '$ a\FINISH'" | tee /tmp/ping_res.txt
