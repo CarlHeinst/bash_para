098ffd1a0fc9b6a1fe23961129d74ad41d8e4860
CarlHeinst <cheinst@gmail.com>
2017-06-21 12:41:21 +0100

    Update paraping.sh
    Added line wraps for greater visibility / readability.

--- paraping.sh
+++ paraping.sh
@@ -6,4 +6,6 @@ echo '
 8.8.8.8
 8.8.4.4
 AS MANY OTHER IPS as you like
-' | sed 's/0.0.0.0\n\|0.0.0.0//g' | xargs -n 1 -P $para_count -I{} bash -c "ping {} -c $ping_count | sed -e '1i\START={}' -e '$ a\FINISH'" | tee /tmp/ping_res.txt
+' | sed 's/0.0.0.0\n\|0.0.0.0//g' \
+| xargs -n 1 -P $para_count -I{} bash -c "ping {} -c $ping_count \
+| sed -e '1i\START={}' -e '$ a\FINISH'" | tee /tmp/ping_res.txt
