845eb945f552f86170dddc6bc3b6ce6ac506f2a7
CarlHeinst <cheinst@gmail.com>
2017-06-21 12:44:39 +0100

    Update mtr_para.sh

--- mtr_para.sh
+++ mtr_para.sh
@@ -1,7 +1,11 @@
+para_count=10
+## This is the number of paralell processes to instruct Xargs to make use of
+mtr_count=50
+## This is the number of pings to carry out
 echo '
 8.8.8.8
 8.8.4.4
 ANY OTHER IPS
 ' | sed 's/0.0.0.0\n\|0.0.0.0//g' \
-| xargs -n 1 -P 10 -I{} bash -c "mtr {} -rw -c ${mtr_count} | sed -e '1i\START={}' -e '$ a\FINISH'" \
+| xargs -n 1 -P $para_count -I{} bash -c "mtr {} -rw -c $mtr_count | sed -e '1i\START={}' -e '$ a\FINISH'" \
 | tee /tmp/${exec_id}/mtr.txt
